d5b47e56fe78c27426a7a37aeea5de64bf9ffd08
Jack Makiyama <jack@phpbr.org>
2015-03-22 23:04:52 -0300

    Configured http request on index.

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
+use Zend\Http\Request;
+use Zend\Http\Headers;
 use Phrases\Application;
 use Phrases\Persistence;
+use Phrases\Entity\Phrase;
 use Phrases\Http\Response\Send;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$persistance = new Persistence\Memory(['Jack Makiyama']);
-$app = new Application($persistance);
+$phrase = new Phrase('Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.');
+$persistance = new Persistence\Memory([$phrase]);
+
+$request = new Request;
+$request->setMethod('GET');
+$request->setUri('http://localhost/');
+$request->setHeaders(Headers::fromString('Accept: plain/text'));
+
+$app = new Application($persistance, $request);
 $response = $app->fetchResponse();
 
 Send::response($response);
